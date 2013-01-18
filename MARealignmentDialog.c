bf0dca34dc782710bb2d9ee8a7008aa00065a0b6
Bill Hill <Bill.Hill@igmm.ed.ac.uk>
2013-01-18 12:17:36 +0000

    Followed changes to WlzEffReadObj() parameters.

--- MARealignmentDialog.c
+++ MARealignmentDialog.c
@@ -500,7 +500,7 @@ void realignReadOverlayCb(
   }
 
   /* read the new source object - ready to extend to other types */
-  obj = WlzEffReadObj(fp, NULL, WLZEFF_FORMAT_WLZ, 0, &errNum);
+  obj = WlzEffReadObj(fp, NULL, WLZEFF_FORMAT_WLZ, 0, 0, 0, &errNum);
   fclose(fp);
 
   /* check the object */
