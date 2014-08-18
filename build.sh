9f65b2c6a1027f886172efde7cbaba2e9b00f3f2
Bill Hill <Bill.Hill@igmm.ed.ac.uk>
2014-08-18 12:01:31 +0100

    Added build with static libs.

--- build.sh
+++ build.sh
@@ -16,9 +16,13 @@ autoreconf
 export MA=/opt/MouseAtlas
 
 # Configure
+#./configure --with-pic --enable-optimise --enable-openmp \
+#            --with-woolz=$MA --with-nifti=$MA --with-tiff=$MA --with-jpeg=$MA \
+#            --with-xbae=$MA --with-hgux=$MA
 ./configure --with-pic --enable-optimise --enable-openmp \
-            --with-woolz=$MA --with-nifti=$MA --with-tiff=$MA --with-jpeg=$MA \
-            --with-xbae=$MA --with-hgux=$MA
+            --with-static-woolz=$MA --with-static-nifti=$MA \
+	    --with-static-tiff=$MA --with-static-jpeg=$MA \
+	    --with-static-xbae=$MA --with-static-hgux=$MA
 
 # Make and install
 make
